583b4b090a2a26b08e434a9dee6815530b580512
KAH HOWE WOO <46815334+JustAyce@users.noreply.github.com>
2022-10-18 22:26:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #/bin/bash
-/home/vagrant/3204/ELK/setup-elk.sh
-/home/vagrant/3204/snort/setup-snort.sh
+/home/vagrant/3204/ELK/build-elk.sh
+/home/vagrant/3204/snort/build-snort.sh
+/home/vagrant/3204/vyos/build-vyos.sh
 echo Done
