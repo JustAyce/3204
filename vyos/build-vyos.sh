2b742571670864cc459a2c7b67df4857c25c70fd
JustAyce <kahowe1woo@gmail.com>
2022-10-19 00:36:18 +0800

    you can start testing vyos already

--- vyos/build-vyos.sh
+++ vyos/build-vyos.sh
@@ -8,13 +8,20 @@ sudo apt-get install -y squashfs-tools
 snap install docker
 mkdir /home/vagrant/vyos/unsquashfs
 sudo unsquashfs -f -d unsquashfs/ rootfs/live/filesystem.squashfs
-# sudo tar -C unsquashfs -c . | sudo docker import - vyos:1.4-rolling-202209260217
-sudo tar -zcvf test.tar.gz unsquashfs && sudo docker import test.tar.gz vyos:1.4-rolling-202111281249
+sudo tar -C unsquashfs -c . | sudo docker import - vyos:1.4-rolling-202209260217
+# sudo tar -zcvf test.tar.gz unsquashfs && sudo docker import test.tar.gz vyos:1.4-rolling-202111281249
 sudo umount rootfs
 cd ..
 # sudo rm -rf vyos
 sudo docker run -d --rm --name vyos --privileged -v /lib/modules:/lib/modules vyos:1.4-rolling-202209260217 /sbin/init
+
+# Run line 22 to get a shell of the vyos router and execute commands directly
+# remember you need to configure to go to enable mode like cisco router.
+# Before commiting, you can run the command "compare to see the changes made, additions and deletions"
+# Once done run the command commit to commit the changes and save to save it
 # sudo docker exec -ti vyos su - vyos
-sudo docker cp /home/vagrant/3204/vyos/configure-vyos.sh vyos:/home/vyos/setup-vyos-config.sh
-sudo docker exec vyos sudo chown -R root:vyattacfg /opt/vyatta/config/active
-sudo docker exec vyos chmod +x /home/vyos/setup-vyos-config.sh && /home/vyos/setup-vyos-config.sh
+
+# This part works when i key in manually but not in script, idk why, pls try
+# sudo docker cp /home/vagrant/3204/vyos/configure-vyos.sh vyos:/home/vyos/setup-vyos-config.sh
+# sudo docker exec vyos sudo chown -R root:vyattacfg /opt/vyatta/config/active
+# sudo docker exec vyos chmod +x /home/vyos/setup-vyos-config.sh && /home/vyos/setup-vyos-config.sh
