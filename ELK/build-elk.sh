a505ecbbed4535d0a5c061853ab50bc59c45a4a6
JustAyce <kahowe1woo@gmail.com>
2022-10-19 12:10:13 +0800

    attempting to use docker-compose instead cuz easier to read/code/debug

--- ELK/build-elk.sh
+++ ELK/build-elk.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 cd /home/vagrant/3204/ELK/
-docker build . -t justayce/elk
-docker run -d --name docker-elk justayce/elk
+# docker build . -t justayce/elk
+# docker run -d --name docker-elk justayce/elk
+docker-compose up -d 
