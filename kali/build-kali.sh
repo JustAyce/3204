9fad22cee0d6f4c5359f3477b29e6885a05b09c3
Nigouz <75168577+Nigouz@users.noreply.github.com>
2022-10-23 18:07:47 +0800

    Update build-kali.sh

--- kali/build-kali.sh
+++ kali/build-kali.sh
@@ -1,3 +1,3 @@
-sudo docker build . -t justayce/kali
+sudo docker build /home/vagrant/3204/kali/Dockerfile -t justayce/kali
 sudo docker run -d --rm --network elk_elk justayce/kali --name KALI
 sudo docker exec --name KALI python3 -m http.server 80 #didn't bind the IP cause I'm not sure how to get docker's IP before this. Tested with 0.0.0.0 and it works
