2a3c9afd69a094aaf373e560a1549c37177b682e
Nigouz <75168577+Nigouz@users.noreply.github.com>
2022-10-24 16:00:33 +0800

    Update build-kali.sh
    It better not die on me

--- kali/build-kali.sh
+++ kali/build-kali.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 sudo docker build 3204/kali -t justayce/kali
-sudo docker run -d --network elk_elk --name KALI justayce/kali
+sudo docker run -d --network elk_elk --name KALI -p 80:80 justayce/kali
 sudo docker exec -d --name KALI python3 -m http.server 80
