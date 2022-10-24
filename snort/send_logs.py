8304a8eb71e7f3e0214d9811b1dc5cefe71452d9
KAH HOWE WOO <46815334+JustAyce@users.noreply.github.com>
2022-10-24 09:50:44 +0800

    Update send_logs.py

--- snort/send_logs.py
+++ snort/send_logs.py
@@ -3,20 +3,22 @@ import subprocess
 import select
 import es_client
 
-f = subprocess.Popen(['tail','-F','/var/log/snort/snort.alert.fast'],\
-        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-p = select.poll()
-p.register(f.stdout)
-
+# Create connection to es
 ec = es_client.ElasticClient()
 if ec.connect():
   print("Connected")
 else:
   exit(0)
 
+# Reads the log output by snort and inserts each row of data to elasticsearch
+f = subprocess.Popen(['tail','-F','/var/log/snort/snort.alert.fast'],\
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+p = select.poll()
+p.register(f.stdout)
+
 while True:
     if p.poll(1):
         alert = f.stdout.readline()
         print(alert)
-				# ec.insert(alert)
+	ec.insert(alert)
         time.sleep(1)
