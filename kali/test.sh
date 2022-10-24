2d54b93a9fbaaa51e4bf5c0a3ac1b563ab30abf0
Nigouz <75168577+Nigouz@users.noreply.github.com>
2022-10-25 03:24:37 +0800

    idk what im doing

--- kali/test.sh
+++ kali/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Start the first process
-python3 /opt/hoaxshell/hoaxshell.py -o -s 10.0.0.100 &
+exec python3 /opt/hoaxshell/hoaxshell.py -o -s 10.0.0.100 &
   
 # Start the second process
 /opt/HTTPUploadExfil/httpuploadexfil :1337 /home/vagrant/exfildata &
   
 # Wait for any process to exit
-wait -n
+#wait -n
   
 # Exit with status of process that exited first
-exit $?
+#exit $?
